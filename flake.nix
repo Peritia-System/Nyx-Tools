2f9ff34ad4cace211b2e0409d4f6638051e64bd1
Peritia <Peritia@peritia.space>
2025-08-06 09:34:26 +0200

    Adding Importing functionality

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "Nyx: Home Manager Tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    homeManagerModules.default = import ./nyx;
+  };
+}
